d0614bb6216e1f0b4875cd354ddeadab684e772c
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-07-17 11:17:27 -0700

    Setup and test search bar and table view with fake results

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Khalid Alrashed on 7/17/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
