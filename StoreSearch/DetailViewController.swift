a182e736aa8826ac190136426638903cd15a4f75
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-07-18 14:55:48 -0700

    Present and dismiss pop-up view with custom animation

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -94,6 +94,14 @@ extension DetailViewController: UIViewControllerTransitioningDelegate {
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController: presented, presenting: presenting)
     }
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return BounceAnimationController()
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return SlideOutAnmationController()
+    }
 }
 
 extension DetailViewController: UIGestureRecognizerDelegate {
