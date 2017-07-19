943bbdb546e015a9698ab465c8fad02d4d4e6fe7
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-07-19 16:46:04 -0700

    Setup split-view for iPad

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -24,6 +24,9 @@ class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = NSLocalizedString("Search", comment: "Split-view master button")
+        
         searchBar.becomeFirstResponder()
         tableView.contentInset = UIEdgeInsets(top: 108, left: 0, bottom: 0, right: 0)
         tableView.rowHeight = 80
