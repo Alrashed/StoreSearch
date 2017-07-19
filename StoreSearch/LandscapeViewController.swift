6020b38a77b7c85d21085d4e2f974a2d2db3539a
Khalid Alrashed <Alrashed.kh@gmail.com>
2017-07-18 18:01:28 -0700

    Setup landscape mode with crossfade animation

---
+++ StoreSearch/LandscapeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LandscapeViewController.swift
+//  StoreSearch
+//
+//  Created by Khalid Alrashed on 7/18/17.
+//  Copyright © 2017 Khalid Alrashed. All rights reserved.
+//
+
+import UIKit
+
+class LandscapeViewController: UIViewController {
+    
+    deinit {
+        print("deinit \(self)")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
